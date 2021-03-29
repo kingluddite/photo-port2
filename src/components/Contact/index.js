099d2f4e81c8261a1283e0f830ad3580bad90080
philip howley <howley.phil@gmail.com>
2021-03-29 14:40:09 -0400

    Add ContactForm with as a controlled component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,45 @@
+import { useState } from 'react';
+
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const { name, email, message } = formState;
+
+  function handleChange(event) {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  }
+
+  // console.log(formState);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formState);
+  }
+
   // JSX
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name: </label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={name} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={email} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" rows="5"></textarea>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          ></textarea>
         </div>
         <button type="submit">Submit</button>
       </form>
